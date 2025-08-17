ab35634de5e37e49edd923eef77fc5b6d615339c
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2025-08-18 00:41:59 +0200

    test esling v10

--- react-frontend/eslint.config.js
+++ react-frontend/eslint.config.js
@@ -24,5 +24,6 @@ module.exports = {
     'react/jsx-uses-vars': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
   },
 };
