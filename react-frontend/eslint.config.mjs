47c86b2ce9479d8ca9d7821a0366f7a0c002cad0
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2025-08-17 19:49:41 +0200

    update eslint config

--- react-frontend/eslint.config.mjs
+++ react-frontend/eslint.config.mjs
@@ -1,10 +1,11 @@
+// eslint.config.mjs
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
     plugins: {
       js,
       react: pluginReact,
@@ -22,10 +23,12 @@ export default [
       "react/react-in-jsx-scope": "off",
       "constructor-super": "off",
       "react/jsx-uses-vars": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
     },
   },
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
     ...pluginReact.configs.flat.recommended,
     settings: {
       react: {
