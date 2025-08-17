7f8f44fbe7ad6149a905942da28e7f06f61b5f95
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2025-08-17 20:02:00 +0200

    test eslint v7

--- react-frontend/eslint.config.js
+++ react-frontend/eslint.config.js
@@ -1,40 +1,32 @@
-const js = require("@eslint/js");
-const globals = require("globals");
-const pluginReact = require("eslint-plugin-react");
-const pluginReactHooks = require("eslint-plugin-react-hooks");
+const js = require('@eslint/js');
+const globals = require('globals');
+const pluginReact = require('eslint-plugin-react');
+const pluginReactHooks = require('eslint-plugin-react-hooks');
 
-module.exports = [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
-    plugins: {
-      js,
-      react: pluginReact,
-      "react-hooks": pluginReactHooks,
-    },
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.jest,
-        structuredClone: "readonly",
-        $: "readonly",
-      },
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-      "constructor-super": "off",
-      "react/jsx-uses-vars": "error",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx,ts,tsx}"],
-    ...pluginReact.configs.flat.recommended,
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-vars': 'error',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-];
+};
