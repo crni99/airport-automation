5854bd4d13cee8dda2bcf371a24ee20179e09aa8
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2025-08-28 17:05:28 +0200

    Update BaseController on MVC to Handle if User is Not Logged

--- mvc-frontend/AirportAutomationWeb/Controllers/BaseController.cs
+++ mvc-frontend/AirportAutomationWeb/Controllers/BaseController.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AirportAutomation.Web.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace AirportAutomation.Web.Controllers
 {
 	public class BaseController : Controller
 	{
-		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		public override void OnActionExecuting(ActionExecutingContext context)
 		{
-			base.OnActionExecuting(filterContext);
+			base.OnActionExecuting(context);
 
 			ViewBag.ApiUserRole = HttpContext.Session.GetString("AccessRole");
+
+			string controllerName = context.RouteData.Values["controller"]?.ToString();
+			if (string.Equals(controllerName, "Home", StringComparison.OrdinalIgnoreCase))
+				return;
+
+			string token = HttpContext.Session.GetString("BearerToken");
+
+			if (string.IsNullOrEmpty(token))
+			{
+				var alertService = HttpContext.RequestServices.GetService(typeof(IAlertService)) as IAlertService;
+				if (alertService != null)
+				{
+					alertService.SetAlertMessage(TempData, "unauthorized_access", false);
+				}
+				context.Result = new RedirectToActionResult("Index", "Home", null);
+			}
 		}
 	}
 }
