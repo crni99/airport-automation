9ac28e1be8740d3316d99cc105e7a17593e084e0
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2025-10-01 01:33:35 +0200

    Update DataContext.jsx

--- react-frontend/src/store/DataContext.jsx
+++ react-frontend/src/store/DataContext.jsx
@@ -1,6 +1,8 @@
 import { createContext } from "react";
 
+const defaultApiUrl = "https://localhost:44362/api/v1";
+
 export const DataContext = createContext({
-    apiUrl: "https://localhost:44362/api/v1",
+    apiUrl: process.env.REACT_APP_API_URL || defaultApiUrl,
     pageSize: 10,
 });
